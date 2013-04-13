109100d389f1000cabdf3edba61dc9c5e752e27a
Romain Ardiet <romain.ardiet@gmail.com>
2013-04-13 17:24:14 +0200

    remove async module + add countWordOccurencesInContentPad() function

--- routes/words.js
+++ routes/words.js
@@ -37,22 +37,41 @@ exports.list = function(req, res) {
 exports.detail = function(req, res, next) {
   var word = req.params.word;
   var etherpad = req.app.get('etherpad');
+  var occurences = 1; // init occurences variable
   etherpad.listAllPads(function(err, data) {
     if (err) return next(err.message);
-    async.forEach(data.padIDs, function (padID, callback) {
-      var Request = [];
-      Request.padID = padID;
+    data.padIDs.forEach(function(padID){
+      var rev = [];
+      rev.padID = padID;
       //console.log(util.inspect(arrayTest));
-      etherpad.getText(arrayTest, function(err, data) {
+      etherpad.getText(rev, function(err, data) {
         if (err) return next(err.message);
-        // Display content
-        console.log(data);
+        occurences += countWordOccurencesInContentPad(data, word); // increments number of occurences for each pad
+        console.log(occurences);
       });
-    },
-    function(err) {
-      return next(err.message);
     });
-
-    //res.json(data.padIDs);
+  });
+  res.send({
+    word: {
+      "occurences": occurences
+    }
   });
 };
+
+/**
+* count the occurence of the word in the content with a regexp
+*
+* @param object content
+* @param string word
+* @return int number of occurences
+*/
+function countWordOccurencesInContentPad(content, word) {
+  var text = content.text;
+  var regex = new RegExp( word, 'g' ); // g = search in global text
+  var count = text.match(regex); // match the regex
+  if (count !== null) { // if results exists
+    return count.length; // return number of occurences
+  } else {
+    return 0; // 0 occurence of the word in the pad
+  }
+}
