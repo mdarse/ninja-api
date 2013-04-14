e527ce609e29b543f82dd07fcd63aff765a60654
Mathieu Darse <mattdarse@gmail.com>
2013-04-15 00:49:14 +0200

    Implement ranking

--- routes/words.js
+++ routes/words.js
@@ -1,16 +1,18 @@
 var WordCounter = require('../lib/wordcounter.js');
 var removeDiacitics = require('../lib/remove-diacritics.js');
 
-exports.top = function(req, res) {
-  res.json({
-    "ninja": {
-      "occurrences": 125,
-      "rank": 1
-    },
-    "saucisse": {
-      "occurrences": 76,
-      "rank": 2
-    }
+exports.top = function(req, res, next) {
+  var limit = Math.min(
+    parseInt(req.query.limit, 10) || 10,
+    100
+  ); // Limit to 100 items
+  var etherpad = req.app.get('etherpad');
+
+  getAllPads(etherpad, function(err, pads) {
+    if (err) return next(err);
+
+    var ranking = makeRanking(pads, limit);
+    res.json(ranking);
   });
 };
 
@@ -79,6 +81,50 @@ function getAllPads(etherpadClient, callback) {
 }
 
 
+var wordRegEx = /[\wÀ-ÿ\-'’]+/gi;
+
+function makeRanking(pads, size) {
+  var words = {};
+
+  pads.forEach(function(pad) {
+    // Find words
+    var matches = pad.text.match(wordRegEx);
+    matches.forEach(function(match) {
+      // Accumulate words couting occurrences
+      var canonical = canonicalWord(match),
+          word = words[canonical];
+      if (!word) {
+        word = words[canonical] = {
+          occurrences: 0,
+          rank: null,
+          forms: []
+        };
+      }
+      word.occurrences++;
+      // Keep various forms (with diacritics, etc)
+      if (word.forms.indexOf(match) === -1)
+        word.forms.push(match);
+    });
+  });
+
+  // Sort words
+  var sortable = [];
+  for (var key in words)
+    sortable.push([words[key].occurrences, key]);
+  sortable.sort(function(a, b) {
+    return b[0] - a[0]; // reverse sorting order
+  });
+
+  // Build ranking
+  var ranking = {};
+  sortable.slice(0, size).forEach(function(item, index) {
+    var key = item[1],
+        word = words[key];
+    word.rank = index + 1;
+    ranking[key] = word;
+  });
+  return ranking;
+}
 
 
 function canonicalWord(word) {
